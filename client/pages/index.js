85f6f92b85f4eb7f0ac4b3e9720c88ee260f4016
jumadhie <jumadhie@gmail.com>
2023-04-18 04:20:03 +0700

    update UI

--- client/pages/index.js
+++ client/pages/index.js
@@ -19,16 +19,40 @@ export default function Home() {
   };
 
   return (
-    <div className={styles.container}>
-      <input
-        placeholder="kirim pesan"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      ></input>
-      <button onClick={sendMessage}>Send Message</button>
-      {chat.map((d, i) => {
-        return <div key={i}>{d.pesan}</div>;
-      })}
+    <div
+      style={{
+        width: "50%",
+        margin: "0 auto",
+        background: "#f7f8fc",
+        height: "95vh",
+        padding: 24,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <div style={{ background: "#f7f8fc", flex: 1 }}>
+        {chat.map((d, i) => {
+          return <div key={i}>{d.pesan}</div>;
+        })}
+      </div>
+      <div
+        style={{
+          background: "white",
+          display: "flex",
+          margin: "0 auto",
+          width: "100%",
+          // marginBottom: 32,
+        }}
+      >
+        <input
+          style={{ width: "100%" }}
+          placeholder="kirim pesan"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></input>
+        <button onClick={sendMessage}>Send Message</button>
+      </div>
     </div>
   );
 }
